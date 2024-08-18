9e3cc8715f31bc91a4c7a9fa8d67a75de84229cb
Aakash Tembhare <82498272+21-aakash@users.noreply.github.com>
2024-08-18 10:02:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 # Streamlit app ki settings set karenge
-st.markdown('<h1 class="main-title">Text To Math Problem Solver And Data Search Assistant</h1>', unsafe_allow_html=True)
+st.markdown('<h1 class="main-title">👽Text To Math Problem Solver And Data Search Assistant</h1>', unsafe_allow_html=True)
 st.markdown('<h2 class="subheader">Text To Math Problem Solver Using Google Gemma 2</h2>', unsafe_allow_html=True)
 
 # Groq API key ko environment variables se load karenge
